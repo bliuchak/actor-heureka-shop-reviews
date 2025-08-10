9722c4991efb0e3ee3db655c7ceb55816cedcbd5
Yurii Bliuchak <1957659+bliuchak@users.noreply.github.com>
2025-08-10 17:26:33 +0200

    chore: format code

--- scripts/fakeserver.ts
+++ scripts/fakeserver.ts
@@ -11,9 +11,9 @@ const __dirname = path.dirname(__filename);
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+    console.log(`Server running at http://localhost:${PORT}`);
 });
--- src/main.ts
+++ src/main.ts
@@ -7,17 +7,17 @@ interface Input {
 }
 
 interface Output {
-    author: string,
-    reviewAt?: string,
-    recommendation: string,
-    rating?: string,
-    pros?: string[],
-    cons?: string[],
-    summary?: string
+    author: string;
+    reviewAt?: string;
+    recommendation: string;
+    rating?: string;
+    pros?: string[];
+    cons?: string[];
+    summary?: string;
     shopReply?: {
-        title: string,
-        body: string
-    } | null
+        title: string;
+        body: string;
+    } | null;
 }
 
 await Actor.init();
@@ -32,7 +32,7 @@ const crawler = new CheerioCrawler({
     maxRequestsPerCrawl,
     sameDomainDelaySecs: 2,
     requestHandler: async ({ enqueueLinks, request, $, log }) => {
-        const currentActivePage = $('li > span.c-pagination__link.is-active').text().trim()
+        const currentActivePage = $('li > span.c-pagination__link.is-active').text().trim();
         log.info(`URL: ${request.url}, page: ${currentActivePage}`);
 
         await enqueueLinks({
@@ -47,30 +47,44 @@ const crawler = new CheerioCrawler({
         reviewsContainer.find('li.c-box-list__item.c-post').each((_, element) => {
             const reviewElement = $(element);
 
-            const author = reviewElement.find('.c-post__author').find('svg').remove().end().text().replace(/\u00a0/g, ' ').trim();
+            const author = reviewElement
+                .find('.c-post__author')
+                .find('svg')
+                .remove()
+                .end()
+                .text()
+                .replace(/\u00a0/g, ' ')
+                .trim();
             const reviewAt = reviewElement.find('.c-post__time-shop > time.c-post__publish-time').attr('datetime');
             const recommendation = reviewElement.find('.c-post__recommendation').text().trim();
             const rating = reviewElement.find('.c-rating-widget').attr('data-rating');
             const summary = reviewElement.find('p.c-post__summary').text().trim();
 
-            let pros: string[] = []
+            let pros: string[] = [];
 
-            const prosContainer = reviewElement.find('ul.c-attributes-list.c-attributes-list--pros.c-attributes-list--circle.o-block-list.o-block-list--snug');
+            const prosContainer = reviewElement.find(
+                'ul.c-attributes-list.c-attributes-list--pros.c-attributes-list--circle.o-block-list.o-block-list--snug',
+            );
             prosContainer.find('li.c-attributes-list__item').each((_, element) => {
                 pros.push($(element).text().trim());
             });
 
-            let cons: string[] = []
+            let cons: string[] = [];
 
-            const consContainer = reviewElement.find('ul.c-attributes-list.c-attributes-list--cons.c-attributes-list--circle.o-block-list.o-block-list--snug');
+            const consContainer = reviewElement.find(
+                'ul.c-attributes-list.c-attributes-list--cons.c-attributes-list--circle.o-block-list.o-block-list--snug',
+            );
             consContainer.find('li.c-attributes-list__item').each((_, element) => {
                 cons.push($(element).text().trim());
             });
 
-            const title = reviewElement.find('.c-post-response > h3.c-post-response__heading.e-heading > span').text().trim();
+            const title = reviewElement
+                .find('.c-post-response > h3.c-post-response__heading.e-heading > span')
+                .text()
+                .trim();
             const body = reviewElement.find('.c-post-response > p').text().trim();
 
-            const shopReply = (title || body) ? { title, body } : null
+            const shopReply = title || body ? { title, body } : null;
 
             const pageReview: Output = {
                 author,
@@ -80,8 +94,8 @@ const crawler = new CheerioCrawler({
                 pros,
                 cons,
                 summary,
-                shopReply
-            }
+                shopReply,
+            };
 
             pageReviews.push(pageReview);
         });
